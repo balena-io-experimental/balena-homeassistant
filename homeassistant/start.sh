064ed1c10125edee9586fd8b9c16be1cfd5ab2a1
Nathan Kuik <nathan@nathankuik.com>
2021-01-10 16:43:48 +0100

    Revert "Try adding pihole"

--- homeassistant/start.sh
+++ homeassistant/start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-cp /tmp/configuration.yaml /config/configuration.yaml
-cp -R /tmp/custom_components /config/
-cp -R /tmp/themes /config/
-
-cp /tmp/sensors.yaml /config/sensors.yaml
+cp /tmp/sense.yaml /config/sense.yaml
 cp /tmp/sensors.sh /config/sensors.sh
 cp /tmp/lovelace.sh /config/lovelace.sh
 
 chmod +x /config/sensors.sh
 chmod +x /config/lovelace.sh
 
-if [ ! -f /config/ui-lovelace.yaml ]; then cp /tmp/ui-lovelace.yaml /config/ui-lovelace.yaml; fi
+if [ ! -f /config/ui-lovelace.yaml ]; then cp /tmp/ui-lovelace.yaml /config/ui-lovelace.yaml; fi 
 
 python -m homeassistant --config /config
